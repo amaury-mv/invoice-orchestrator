271d5a72c38fb786f575a996c883aaa447254133
Amaury Martinez <amaury.mv2107@gmail.com>
2023-10-16 13:07:05 -0600

    first commit

---
+++ src/main/java/com/eis/invoiceorchestrator/InvoiceOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.eis.invoiceorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eis/invoiceorchestrator/InvoiceOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eis.invoiceorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
